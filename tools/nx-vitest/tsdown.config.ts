3e47c11c5a0ad2d3c8c8ae36a6d95adb2d1217a6
Petr Plenkov <petr.plenkov@booking.com>
2025-10-05 00:58:23 +0200

    chore: move nx plugins to local tools and update build process

---
+++ tools/nx-vitest/tsdown.config.ts
@@ -0,0 +1,9 @@
+// tsdown.config.ts
+import { defineConfig } from 'tsdown';
+
+export default defineConfig({
+  sourcemap: true,
+  tsconfig: 'tsconfig.lib.json',
+  skipNodeModulesBundle: true,
+  format: 'cjs',
+});
